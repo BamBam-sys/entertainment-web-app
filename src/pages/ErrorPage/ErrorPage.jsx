b14dff0b6b5bccfdaead202048390405c9e29db4
BamBam-sys <lawalayobamidavid@gmail.com>
2022-06-16 22:43:30 +0100

    added error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './errorPage.scss';
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="errorPage">
+      <h2>
+        This page does not exist, go back to{' '}
+        <Link to={'/'} className="link">
+          Home page
+        </Link>
+      </h2>
+    </div>
+  );
 };
 
 export default ErrorPage;
