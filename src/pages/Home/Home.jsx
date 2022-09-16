6fb638e703122d50278cb9d3dbea6c985a6fd3d8
BamBam-sys <lawalayobamidavid@gmail.com>
2022-09-16 21:10:32 +0200

    update readme file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -65,3 +65,16 @@ const Home = () => {
 };
 
 export default Home;
+
+//todo
+/*1. each user signed up user should have their own list of bookmarked shows 
+  - in the database, each user should have an array of bookmarked shows linked to the user.
+  - when the user logs in, user's list of bookmarked shows will be fetched, the list of all shows will then be looped through,
+     and for every show bookmarked by the user, same show's "isBookmarked" property will be set to true.
+
+  2. when bookmarking a show to enusure that the visual indication of a show being bookmarked happens almost instantly,
+    the action should be dispatched to the store first before the call to the database is made and if for any reason the call to 
+    the database fails, the dispactched action should be rolled back.
+  
+  3. create pop-up modals for errors.
+*/
